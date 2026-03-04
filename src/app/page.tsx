ac5154ae77ffe57f15be5bbcb631dda68d1a1fd0
wagner <wagnerparnoffpereira@gmail.com>
2026-03-04 14:17:19 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div className="text-red-500 font-bold">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main landing page of the application.</p>
+    </div>
+  );
+};
+
+export default Page;
