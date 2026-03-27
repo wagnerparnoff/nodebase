571959276144e8e8caffaaf739ae2cfd8239ede0
wagner <wagnerparnoffpereira@gmail.com>
2026-03-27 19:43:17 -0300

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
 
-const Page = () => {
-
+const Page = async () => {
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button>Click me</Button>
     </div>
   );
 };
