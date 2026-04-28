c3a04f0aaa53682a9d35af80e52f24ca321bb39d
wagner <wagnerparnoffpereira@gmail.com>
2026-04-28 15:53:37 -0300

    06: background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,21 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import { prisma } from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({});
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany({});
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "app/task.created",
+      data: {
+        hello: "world hahaha",
+        id: "test-id"
+      },
+    });
+
+    return { success: true, message: "Workflow created and event sent" };
   }),
 });
 // export type definition of API
